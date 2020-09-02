0da73a7c0b431cce7b516049083b4d05519ee991
“Akash” <Akash@breezeware.net>
2020-09-02 14:19:29 +0530

    Inital commit

---
+++ AkashSwiftLibrary/AkashSwiftLibrary.h
@@ -0,0 +1,19 @@
+//
+//  AkashSwiftLibrary.h
+//  AkashSwiftLibrary
+//
+//  Created by Akashchellakumar on 02/09/20.
+//  Copyright © 2020 Breezeware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AkashSwiftLibrary.
+FOUNDATION_EXPORT double AkashSwiftLibraryVersionNumber;
+
+//! Project version string for AkashSwiftLibrary.
+FOUNDATION_EXPORT const unsigned char AkashSwiftLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AkashSwiftLibrary/PublicHeader.h>
+
+
