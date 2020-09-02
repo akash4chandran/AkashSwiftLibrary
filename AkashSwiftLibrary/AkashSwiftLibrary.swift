aa3f1a1f32860b26082b70c9824ea0679cb22e68
“Akash” <Akash@breezeware.net>
2020-09-02 16:53:27 +0530

    Added test cases

---
+++ AkashSwiftLibrary/AkashSwiftLibrary.swift
@@ -0,0 +1,23 @@
+//
+//  AkashSwiftLibrary.swift
+//  AkashSwiftLibrary
+//
+//  Created by Akashchellakumar on 02/09/20.
+//  Copyright © 2020 Breezeware. All rights reserved.
+//
+
+import Foundation
+
+public final class AkashSwiftLibrary {
+
+    let name = "AkashSwiftLibrary"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- AkashSwiftLibraryTests/AkashSwiftLibraryTests.swift
+++ AkashSwiftLibraryTests/AkashSwiftLibraryTests.swift
@@ -10,25 +10,40 @@ import XCTest
 @testable import AkashSwiftLibrary
 
 class AkashSwiftLibraryTests: XCTestCase {
+    
+    
+    var akashSwiftLibrary: AkashSwiftLibrary!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        akashSwiftLibrary = AkashSwiftLibrary()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(akashSwiftLibrary.add(a: 1, b: 1), 2)
     }
+    
+    func testSub() {
+           XCTAssertEqual(akashSwiftLibrary.sub(a: 2, b: 1), 1)
+       }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
